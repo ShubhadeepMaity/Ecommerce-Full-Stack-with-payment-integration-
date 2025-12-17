f98f6522e9bc046a393203d35c3640753e7b6f3c
ACER <ACER@DESKTOP-74NML8F>
2025-12-17 16:24:26 +0530

    Initial Commit

---
+++ OnlineShoppingApp/src/main/java/com/ecom/model/OrderRequest.java
@@ -0,0 +1,32 @@
+package com.ecom.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequest {
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private String mobileNo;
+
+	private String address;
+
+	private String city;
+
+	private String state;
+
+	private String pincode;
+	
+	private String paymentType;
+	
+	
+}
+	
